7e66b09d323d7b8f722c1dd8545874387382df1a
marci29 <50134254+marci29@users.noreply.github.com>
2024-04-20 23:31:00 +0200

    Create zadatak_7.c

---
+++ zadatak_7.c
@@ -0,0 +1,9 @@
+/* 7 . Prikazan je dio izvornog koda koji koristi TCA0 i PWM (single slope način rada) za generiranje PWM signala s RADNIM OMJEROM 75% na pinu PD2 (korištenjem CMP2 compare kanala). PERIOD = 1ms… Nadopuni kod koristeći hexa vrijednosti… (pogledati primjer u skripti)… (6bodova)
+
+// postavi mod single slope PWM
+// omogući CMP2
+// izračunaj period iz F_CPU = 3333333UL
+// izračunaj vrijednost compare reg
+// poveži cmp2 s PORTD
+// pokreni TCA0
+While(1); */
