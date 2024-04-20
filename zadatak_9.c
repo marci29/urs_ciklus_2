1d64c4590f6ae2eb2afa940564c768dd00dbf141
marci29 <50134254+marci29@users.noreply.github.com>
2024-04-20 23:32:56 +0200

    Create zadatak_9.c

---
+++ zadatak_9.c
@@ -0,0 +1,10 @@
+/* 9. Napisati odsječak koda koji vrši sljedeće funkcije:
+main:
+Postavlja port pin za LED H2 kao izlazni
+Postavlja PA7 pin (KEY1) za prekid na padajući brid
+Omogućuje prekidni sustav
+ISR:
+Uključi LED na 10ms i onda isključi
+Koristiti HEX vrijednosti za upis u registre koji su navedeni u pomoćnoj dokumentaciji. Za pauzu slobodno koristiti funkciju _delay_ms().
+
+--> uz ovo idu neke sheme */
