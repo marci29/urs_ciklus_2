df26afe11410f5cd948a1db5b99848212b185d60
marci29 <50134254+marci29@users.noreply.github.com>
2024-04-20 23:25:36 +0200

    Create zadatak_2.c

---
+++ zadatak_2.c
@@ -0,0 +1,9 @@
+/* 3. Napišite programski kod za inicijalizaciju i pokretanje A/D pretvornika na ATmega4809 mikrokontroleru u sljedećoj konfiguraciji:
+
+	- interni izvor referentnog napona 2.5 V
+	- 10-bitna razlučivost
+	- single-conversion način rada
+	- omogućiti i pokrenuti A/D pretvorbu po završetku inicijalizacije
+
+Napon napajanja mikrokontrolera je 3.3 V. 
+Napomena: potrebno je napisati samo kod za inicijalizaciju i pokretanje ADC-a. Nije potreban kod za očitavanje vrijednosti A/D pretvorbe. */
