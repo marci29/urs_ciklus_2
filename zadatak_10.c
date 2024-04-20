346539cc008d8bbbe94e7873968d02b75631dd1d
marci29 <50134254+marci29@users.noreply.github.com>
2024-04-20 23:38:33 +0200

    Create zadatak_10.c

---
+++ zadatak_10.c
@@ -0,0 +1,6 @@
+/* 10. Nadopunite program za ATMega 4809 mikrokontroler koji prima i obrađuje
+znakove primljene USART3 sučelja (PB1 = RX, brzina komunikacije 115200 bps). 
+Za prijem znakova koristi se prekidni prijenos, pri čemu se primljeni znakovi pohranjuju u FIFO međuspremnik
+veličine 16 okteta, realiziran statičkim globalnim poljem. */
+
+// nemam kod koji treba nadopuniti
